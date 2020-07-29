b0368b51d659539c5dc2c3f033c94b584b1814f0
SpiritOfSea <dpadabed@yandex.by@>
2020-07-29 18:51:02 +0300

    Added:
    - find_multilint_comments function
    
    Edited:
    
    - delete_empty_line function: now correctly works with multiple empty lines and empty lines with spaces.

--- converter.py
+++ converter.py
@@ -17,12 +17,20 @@ def find_singleline_comments(raw_string):  # function that finds and deletes all
     return new_string
 
 
+def find_multiline_comments(raw_string):
+    new_string = raw_string
+    while (re.search(r"(\/\*).*?(\*\/)", new_string, re.DOTALL)) is not None:
+        escaped_group = re.search(r"(\/\*).*?(\*\/)", new_string, re.DOTALL)
+        new_string = new_string[:escaped_group.start()] + new_string[escaped_group.end():]
+    return new_string
+
+
 def delete_empty_lines(raw_string):  # clearing all the empty lines
     new_string = raw_string
 
-    while re.search(r"(\n\n)", new_string, re.MULTILINE) is not None:  # deleting all empty lines in middle
-        escaped_group = re.search(r"(\n\n)", new_string, re.MULTILINE)
-        new_string = new_string[:escaped_group.start()] + "\n" + new_string[escaped_group.end():]
+    while re.search(r"(\n\s).*?(\S)", new_string, re.DOTALL) is not None:  # deleting all empty lines in middle
+        escaped_group = re.search(r"(\n\s).*?(\S)", new_string, re.DOTALL)
+        new_string = new_string[:escaped_group.start()] + "\n" + new_string[escaped_group.end() - 1:]
 
     while new_string[:1] == "\n":  # deleting all empty lines at the beginning
         new_string = new_string[1:]
@@ -41,6 +49,7 @@ def parse(raw_string):  # function that unites all 'parse' methods
     # 3. Clear empty lines
 
     new_string = find_singleline_comments(raw_string)
+    new_string = find_multiline_comments(new_string)
     new_string = delete_empty_lines(new_string)
     return new_string
 
@@ -48,4 +57,15 @@ def parse(raw_string):  # function that unites all 'parse' methods
 if __name__ == '__main__':
     file_to_transfer = "./test.txt"
     initialize(file_to_transfer)
-    print(parse("\n\n\nTest //Test //Test \n\nTest2 //Test2\nTest3//Test3//Test3\n//Test4\n\n\n"))
+    print(parse("\n\n\n"
+                "Test //Test //Test \n\n"
+                ""
+                "Test2 //Test2\n"
+                "/* this is new \n"
+                " commentary \n "
+                "multiline */ \n "
+                "Test3//Test3//Test3\n"
+                "//Test4"
+                ""
+                ""
+                "\n\n\n"))
