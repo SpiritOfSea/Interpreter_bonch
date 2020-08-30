1ba74e7e80317b09cea49805beea63471662120d
SpiritOfSea <dpadabed@yandex.by@>
2020-08-30 17:09:12 +0300

    Added new features, almost finished
    ADDED:
    - FOR, WHILE, DO...WHILE, IF...ELSE support
    - Properly commented

--- translator.py
+++ translator.py
@@ -1,48 +1,55 @@
 #  module that translates source code into ordered list of operations
 
-import variables
-import re
 from utility import *
 
 
 class Translator:
 
     def __init__(self, translating_file):
-        self.OP_LIST = []
-        self.blocks_dict = get_block_dict(translating_file)
-        self.translating_file = translating_file
-        self.unique_counter = 0
+        self.OP_LIST = []  # Generate empty FINAL list of operations
+        self.blocks_dict = get_block_dict(translating_file)  # Generate block map
+        self.translating_file = translating_file  # Source file
+        self.unique_counter = 0  # Unique counter for commands
+
         # OP LIST INSTRUCTIONS SET:
         # SETVAR (var, content) - set var
         # INCRVAR (var) - increase by 1
         # DECRVAR (var) - decrease by 1
+        # WHILE (cond, ID) - start of while with unique ID
+        # ENDOWHILE (ID) - end of while block with uID
+        # FOR ((cond), ID) - start of for block with uID
+        # ENDFOR (ID) - end of FOR block with uID
+        # IF (cond, ID) - start of IF block with uID
+        # ENDIF (ID) - end of IF block with uID
+        # ELSE (ID) - start of ELSE block, uID is same with connected IF
+        # ENDOFELSE (ID) - end of ELSE block with uID
 
-    def translate(self):
+    def translate(self):  # Source translate function, which start recursive loop
         with open(self.translating_file) as file:
             self.block = [line.strip() for line in file]
-        self.OP_LIST, last_OP = self.translate_block(0, len(self.block), 0)
+        self.OP_LIST = self.translate_block(0, len(self.block))  # First "block" consists of whole file
 
-    def translate_block(self, start, end, last_OP):
-        OP_LIST = []
-        cur = start
+    def translate_block(self, start, end):  # Operation that translates 'block', takes start an end of block
+        OP_LIST = []  # Generate block, inner OP_LIST
+        cur = start  # While end of block not reached, continue translation
         while cur < end:
-            cur, temp_OP, last_OP = self.translate_string(self.block[cur], cur, last_OP)
-            print(cur, end)
+            cur, temp_OP = self.translate_string(self.block[cur], cur, OP_LIST)
             for a in temp_OP:
-                OP_LIST.append(a)
-        return OP_LIST, last_OP
+                OP_LIST.append(a)  # We recursively taking string_OP_LIST, adding it to inner block OP_LIST
+        return OP_LIST
 
-    def translate_string(self, trstr, in_point, last_OP):
-        OP_LIST = []
-        out_point = in_point
-        line_number = get_line_number(trstr)
-        trstr = trstr[4:]
-        key = trstr.split(" ")[0]
-        key = key.split(",")[0]
-        if key[-3:-1] == "++" or key[-3:-1] == "--":
+    def translate_string(self, trstr, in_point, block_OP_LIST):
+        OP_LIST = []  # Inner string OP_LIST
+        out_point = in_point  # By default, after completing one string we move to the next one
+        line_number = get_line_number(trstr)  # Getting current line number as integer
+        trstr = trstr[4:]  # Current string, that contains everything but number of string
+        key = trstr.split(" ")[0]  # We trying to get keyword by splitting by space...
+        key = key.split(",")[0]  # ...or by commas, if we have variables list
+
+        if key[-3:-1] == "++" or key[-3:-1] == "--":  # Holding special case with easy +1 or -1
             key = key[:-3]
-        trstr = trstr[len(key):].strip()
-        try:
+        trstr = trstr[len(key):].strip()  # Deleting keyword from string
+        try:  # Also, deleting ; at the end of current string
             if trstr[-1] == ';':
                 trstr = trstr[:-1]
             else:
@@ -51,60 +58,104 @@ class Translator:
         except IndexError as e:
             pass
 
-        if key == "float" or key == "int" or key == "char":
-            if len(trstr.split("=")) != 1:
+        if key == "float" or key == "int" or key == "char":  # If string starts with keywords
+            if len(trstr.split("=")) != 1:  # If we have construction like "type var1, ..., varN = ..."
                 variables = trstr.split("=")[0].strip()
                 content = trstr.split("=")[1].strip()
                 for var in variables.split(','):
-                    OP_LIST.append(('SETVAR', var.strip(), (key, content)))
-            else:
+                    OP_LIST.append(('SETVAR', var.strip(), (key, content)))  # For each operation we add separate command
+            else:  # If we just declare variables
                 for var in trstr.split(','):
                     OP_LIST.append(('SETVAR', var.strip(), key))
-        elif key == 'while':
-            print("T")
-            OP_starting_adress = last_OP
-            cond = '1'
+
+        elif key == 'while':  # Holding of "while" key
+            if block_OP_LIST[-1][0] == "ENDOFDO":  # If while stands right after end of "do" block
+                cond = re.search(r'(\(.*\))', trstr).group(0)[1:-1]  # Catch while conditions in brackets
+                out_point += 3  # Skip empty block
+                unique_counter = block_OP_LIST[-1][-1]  # Set same ID as "do" block has
+                OP_LIST.append(("DOWHILE", cond, unique_counter))  # Place corresponding command
+            else:  # If while stands independant
+                cond = re.search(r'(\(.*\))', trstr).group(0)[1:-1]
+                out_point = self.blocks_dict[line_number+1]
+
+                temp_OP_LIST = self.translate_block(in_point+1, out_point)
+
+                OP_LIST.append(("WHILE", cond, self.unique_counter))
+                for x in temp_OP_LIST:
+                    OP_LIST.append(x)
+                OP_LIST.append(("ENDOFWHILE", self.unique_counter))
+                self.unique_counter += 1  # Continue global ID counter
+
+        elif key == 'for':  # Holding of "for"
+            cond = re.search(r'(\(.*\))', trstr).group(0)[1:-1].split(';')  # Parse (a;b;c) params as list of conditions
             out_point = self.blocks_dict[line_number+1]
 
-            temp_OP_LIST, last_OP = self.translate_block(in_point+1, out_point, last_OP)
+            temp_OP_LIST = self.translate_block(in_point+1, out_point)
 
-            OP_LIST.append(("WHILE", cond, self.unique_counter))
+            OP_LIST.append(("FOR", cond, self.unique_counter))
             for x in temp_OP_LIST:
                 OP_LIST.append(x)
-            OP_LIST.append(("ENDOFWHILE", self.unique_counter))
-            self.unique_counter +=1
-        elif key == 'for':
-            return 0
+            OP_LIST.append(("ENDOFFOR", self.unique_counter))
+            self.unique_counter += 1
+
         elif key == 'if':
-            return 0
+            cond = re.search(r'(\(.*\))', trstr).group(0)[1:-1]
+            out_point = self.blocks_dict[line_number+1]
+
+            temp_OP_LIST = self.translate_block(in_point+1, out_point)
+
+            OP_LIST.append(("IF", cond, self.unique_counter))
+            for x in temp_OP_LIST:
+                OP_LIST.append(x)
+            OP_LIST.append(("ENDOFIF", self.unique_counter))
+            self.unique_counter += 1
+
         elif key == 'else':
-            return 0
+            out_point = self.blocks_dict[line_number+1]
+
+            temp_OP_LIST = self.translate_block(in_point+1, out_point)
+            unique_counter = block_OP_LIST[-1][-1]
+            OP_LIST.append(("ELSE", unique_counter))
+            for x in temp_OP_LIST:
+                OP_LIST.append(x)
+            OP_LIST.append(("ENDOFELSE", unique_counter))
+
         elif key == 'do':
-            return 0
+            out_point = self.blocks_dict[line_number+1]
+            temp_OP_LIST = self.translate_block(in_point+1, out_point)
+
+            OP_LIST.append(("DO", self.unique_counter))
+            for x in temp_OP_LIST:
+                OP_LIST.append(x)
+            OP_LIST.append(("ENDOFDO", self.unique_counter))
+            self.unique_counter += 1
+
         elif key == 'switch':
             return 0
-        else:
-                if trstr[0:2] == '++':
+
+        else:  # If string starts with unknown keyword, place it as var name
+                if trstr[0:2] == '++':  # Increase by 1 operation
                     OP_LIST.append(('INCRVAR', key))
 
-                elif trstr[0:2] == '--':
+                elif trstr[0:2] == '--':  # Decrease by 1 operation
                     OP_LIST.append(('DECRVAR', key))
 
-                elif trstr[0:1] == ',':
+                elif trstr[0:1] == ',':  # If we are working with multiple variables
                     variables = key+trstr.split("=")[0].strip()
                     content = trstr.split("=")[1].strip()
 
                     for var in variables.split(','):
                         OP_LIST.append(('SETVAR', var.strip(), content))
 
-                elif trstr[0:1] == '=':
+                elif trstr[0:1] == '=':  # If we work with single variable
                     content = trstr[1:].strip()
                     OP_LIST.append(('SETVAR', key, content))
         if out_point == in_point:
-            out_point += 1
+            out_point += 1  # Default one-by-one line workout
         else:
-            print('QQ' + str(out_point))
-        return out_point, OP_LIST, last_OP+len(OP_LIST)
+            pass
+        return out_point, OP_LIST
+
 
 if __name__ == '__main__':
     Translator = Translator("example.intpr")
